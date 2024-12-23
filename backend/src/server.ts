23b5a62ecb38fd61240e31a1aac2da8b860e4472
Anthony Wittig <anthonywittig@anthonys-mbp-2.lan>
2024-12-23 10:58:34 -0700

    stash

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 
 dotenv.config();
 
@@ -9,9 +10,14 @@ const port = 5001;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.get("/api/test", (req: Request, res: Response) => {
   res.json({ message: "Backend is working!" });
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 app.listen(port, () => {
